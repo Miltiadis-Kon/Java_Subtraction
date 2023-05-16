1cd4f5bd8589c14db6ef451679ce1358f39d7c10
Miltiadis-Kon <miltiadiskontos@gmail.com>
2023-05-16 21:22:42 +0300

    Update SubtractTest.java
    Update test

--- SubtractTwoIntegers/src/mail/SubtractTest.java
+++ SubtractTwoIntegers/src/mail/SubtractTest.java
@@ -14,8 +14,8 @@ public class SubtractTest {
 	@Test
 	public void testSubtractTwoIntegers() {
 		subtract subTest=new subtract();
-		 assertEquals("POSITIVE", subTest.subtractTwoIntegers(10, 5));
-		 assertEquals("NEGATIVE", subTest.subtractTwoIntegers(5, 10));
+		 assertEquals("POSITIVE", subTest.subtractTwoIntegers(7, 5));
+		 assertEquals("NEGATIVE", subTest.subtractTwoIntegers(5, 8));
 
 	}
 
